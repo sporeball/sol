35251fd09704d8c9a188c67de45e97604f9f3d40
sporeball <sporeballdev@gmail.com>
2022-03-07 21:33:53 -0800

    hello sol!

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/*
+  index.ts
+  sol index file
+  copyright (c) 2022 sporeball
+  MIT license
+*/
+
+export function init() {
+  console.log('hello sol!');
+}
---
+++ src/sol.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/*
+  sol.ts
+  sol CLI entry point
+  copyright (c) 2022 sporeball
+  MIT license
+*/
+
+import * as sol from './index.js';
+
+sol.init();
