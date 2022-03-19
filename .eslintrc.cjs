6b4b52c3666e136d38068a6c707c1295d0e1be52
sporeball <sporeballdev@gmail.com>
2022-03-18 18:12:15 -0700

    linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+// sol sets "type" to "module", so this needs to be a .cjs file
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
